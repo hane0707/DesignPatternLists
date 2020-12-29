a0a7bf1d5df7b7c03b6cbcc79616c3636d66ba43
koob <hidamari4256@gmail.com>
2020-12-29 22:24:44 +0900

    ファクトリーパターンの追加

--- DesignPatternLists/Program.cs
+++ DesignPatternLists/Program.cs
@@ -11,6 +11,7 @@ namespace DesignPatternLists
             string[] arr = new string[] { "鶏むね肉", "カレールー", "じゃがいも", "にんじん", "たまねぎ" };
 
             // イテレータパターン(イテレータブロック未使用)
+            Console.WriteLine("■イテレータパターン(イテレータブロック未使用)");
             var shoppingCartA = new IteratorA.ShoppingCart();
             foreach (var item in arr)
                 shoppingCartA.AppendItem(new IteratorA.ShoppingItem(item));
@@ -19,6 +20,7 @@ namespace DesignPatternLists
                 Console.WriteLine(item.ItemName);
 
             // イテレータパターン(イテレータブロック使用)
+            Console.WriteLine("■イテレータパターン(イテレータブロック使用)");
             var shoppingCartB = new IteratorB.ShoppingCart();
             foreach (var item in arr)
                 shoppingCartB.AppendItem(new IteratorB.ShoppingItem(item));
@@ -27,11 +29,23 @@ namespace DesignPatternLists
                 Console.WriteLine(item.ItemName);
 
             // テンプレートメソッドパターン
+            Console.WriteLine("■テンプレートメソッドパターン");
             List<string> list = arr.ToList();
             var templateMethodA = new TemplateMethod.ConcreteJoinStringWithComma();
             templateMethodA.Display(arr);
             var templateMethodB = new TemplateMethod.ConcreteJoinStringWithSpace();
             templateMethodB.Display(list);
+
+            // ファクトリーパターン
+            Console.WriteLine("■ファクトリーパターン");
+            FactoryFramework.Factory factory = new Idcard.IDCardFactory();
+            FactoryFramework.Product card1 = factory.Create("test1");
+            FactoryFramework.Product card2 = factory.Create("test2");
+            FactoryFramework.Product card3 = factory.Create("test3");
+
+            card1.Use();
+            card2.Use();
+            card3.Use();
 
             Console.ReadLine();
         }
---
+++ Factory/Factory.cs
@@ -0,0 +1,95 @@
+﻿using FactoryFramework;
+using System;
+using System.Collections.Generic;
+
+// 参考：https://qiita.com/ayayo/items/3eece04bbc4b7504179a
+// 実装側は例えばCreate関数さえ呼べば、そのインスタンスに応じたCreateProductが呼び出され、生成される。
+// ＝別の工場を作成する場合でも、抽象クラス（Framework）側の修正が不要。
+
+/// <summary>
+/// ファクトリーパターンのフレームワーク部分
+/// </summary>
+namespace FactoryFramework
+{
+    /// <summary>
+    /// 工場で作られた製品の抽象クラス
+    /// </summary>
+    public abstract class Product
+    {
+        public abstract void Use();
+    }
+
+    /// <summary>
+    /// 製品を作る工場の抽象クラス
+    /// </summary>
+    public abstract class Factory
+    {
+        public Product Create(string name)
+        {
+            Owner owner = new Owner(name);
+            Product product = CreateProduct(owner);
+            RegisterProduct(product);
+            return product;
+        }
+        protected abstract Product CreateProduct(Owner owner);
+        protected abstract void RegisterProduct(Product product);
+    }
+
+    public class Owner
+    {
+        public string Name { get; }
+
+        public Owner(string name)
+        {
+            Name = name;
+        }
+    }
+}
+
+/// <summary>
+/// ファクトリーパターンを実装したIDカード
+/// </summary>
+namespace Idcard
+{
+    /// <summary>
+    /// 工場で作られたIDカードの具象クラス
+    /// </summary>
+    public class IDCard : Product
+    {
+        public Owner IDCardOwner { get; }
+
+        public IDCard(Owner owner)
+        {
+            IDCardOwner = owner;
+            Console.WriteLine(IDCardOwner.Name + "のカードを作ります。");
+        }
+
+        public override void Use()
+        {
+            Console.WriteLine(IDCardOwner.Name + "のカードを使います。");
+        }
+    }
+
+    /// <summary>
+    /// IDカードを作る工場の具象クラス
+    /// </summary>
+    public class IDCardFactory : Factory
+    {
+        public List<Owner> owners { get; }
+
+        public IDCardFactory()
+        {
+            owners = new List<Owner>();
+        }
+
+        protected override Product CreateProduct(Owner owner)
+        {
+            return new IDCard(owner);
+        }
+
+        protected override void RegisterProduct(Product product)
+        {
+            owners.Add(((IDCard)product).IDCardOwner);
+        }
+    }
+}
